007e53a904b19253a8394e21e744b32c879531f8
Tish <tishthegoat@gmail.com>
2024-06-22 18:59:27 -0600

    header

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    colors:{
+      'primary': '#F0BB6F',
+      'secondary': '#2B6145',
+      'third': '#801D02',
+      'text': '#F4FEFD',
+      'green': '#4A611D',
+      'yellow': '#9E650A'
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
