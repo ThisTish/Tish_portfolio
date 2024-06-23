5c4af55d9d4a9c9616d7096e4b7a3e8c4cb54ade
Tish <tishthegoat@gmail.com>
2024-06-23 04:26:24 -0600

    My works formatted, slideshow started

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html"],
+  content: ["./index.html", "./node_modules/flowbite/**/*.js"],
   theme: {
     colors:{
       'primary': '#F0BB6F',
       'secondary': '#2B6145',
       'third': '#801D02',
       'text': '#F4FEFD',
-      'green': '#4A611D',
-      'yellow': '#9E650A'
+      'grass': '#4A611D',
+      'mustard': '#9E650A'
     },
     extend: {
       fontFamily:{
@@ -16,6 +16,8 @@ module.exports = {
       }
     },
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin')
+  ],
 }
 
